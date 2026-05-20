b561a84e18dd93f17912d3d13575ec6edb897256
shuvo chakrabrati <kumarshuvo265@gmail.com>
2026-05-20 16:34:55 +0600

    using defer

--- main.go
+++ main.go
@@ -7,23 +7,52 @@ import (
 )
 
 
+type Result struct {
+	Url string
+	Status string
+	Err error
+}
+
+func checkWebsiteUrl(url string, ch chan Result) {
+	res, err := http.Get(url)
+		if err != nil {
+			
+			ch <- Result{Url: url, Status: "is down", Err: err}
+			return
+		} else {
+			
+			
+			ch <- Result{Url: url, Status: "is up and running", Err: nil}
+			defer res.Body.Close()
+}
+
+}
+
+
 func main(){
 	urls := []string{
 		"https://google.com",
-		"https://github.com",
+		"https://ruchirong.com",
 		"https://stackoverflow.com",
 	}
+
+	ch := make(chan Result)
 
 	start := time.Now()
 
 	for _, url := range urls {
-		res, err := http.Get(url)
-		if err != nil {
-			fmt.Println(url, "is down")
-		} else {
-			fmt.Println(url, "is up")
-			res.Body.Close()
-		}
+		go checkWebsiteUrl(url, ch)
+	}
+
+	for range urls {
+		result := <-ch
+
+		if result.Err != nil {
+			fmt.Println(result.Url, "is down. Error:", result.Err)
+			continue
+		} 
+
+		fmt.Println(result.Url, result.Status)
 	}
 
 	fmt.Println("time taken:", time.Since(start))
