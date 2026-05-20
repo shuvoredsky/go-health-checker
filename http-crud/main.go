ff47fa0a442905df08a3ed6ecde280cc2b7c2e23
shuvo chakrabrati <kumarshuvo265@gmail.com>
2026-05-20 22:44:44 +0600

    add create user post method

--- http-crud/main.go
+++ http-crud/main.go
@@ -6,17 +6,32 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", rootHandler)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/health", healthHandler)
+	mux.HandleFunc("/createUser", createUserHandler)
 
 	fmt.Println("Server is running at port 5000")
-	err := http.ListenAndServe(":5000", nil)
+	err := http.ListenAndServe(":5000", mux)
 	if err != nil {
 		fmt.Println("Server error", err)
 	}
-
 
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, World!")
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Server is up and running!")
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.	StatusMethodNotAllowed)
+		fmt.Fprintln(w, "Method not allowed")
+		return
+	}
 }
